cd3b42cc492777d331802b8e654a019409c78ead
roberthjsb <roberth.salazargmail.com>
2022-05-09 12:16:05 -0400

    actualizacion estilos

--- src/app/todo-list/input-todo/input-todo.component.ts
+++ src/app/todo-list/input-todo/input-todo.component.ts
@@ -3,23 +3,21 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-input-todo',
   templateUrl: './input-todo.component.html',
-  styleUrls: ['./input-todo.component.scss']
+  styleUrls: ['./input-todo.component.scss'],
 })
-export class InputTodoComponent  {
-  @Output() added= new EventEmitter<string>();
-  textValue:string='';
-  constructor() { }
+export class InputTodoComponent {
+  @Output() added = new EventEmitter<string>();
+  textValue: string = '';
+  constructor() {}
 
-  clickAdd(){
+  clickAdd() {
     this.added.emit(this.textValue);
   }
-  isEmpty(){
-    return this.textValue.length===0;
+  isEmpty() {
+    return this.textValue.length === 0;
   }
-  setValue($event:Event): void{
+  setValue($event: Event): void {
     const val = ($event.target as HTMLInputElement).value;
-    this.textValue=val;
+    this.textValue = val;
   }
-  
-
 }
--- src/app/todo-list/todo-list.module.ts
+++ src/app/todo-list/todo-list.module.ts
@@ -4,8 +4,6 @@ import { InputTodoComponent } from './input-todo/input-todo.component';
 import { TodoListPageComponent } from './todo-list-page/todo-list-page.component';
 import { TodoComponent as TodoComponent } from './todo/todo.component';
 
-
-
 @NgModule({
   declarations: [
     InputTodoComponent,
